389cdc1c266aca6489e52763724a966a5d2bd79b
openmpy <suhwan@kakao.com>
2024-09-03 13:58:55 +0900

    chore: Update Header component with logo and styling

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,46 @@
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import { Container } from "../../Container.styles";
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <HeaderWrapper as="header">
+      <Logo>
+        <Link to={"/"}>
+          <img src="./images/logo.svg" alt="logo" />
+        </Link>
+        <h2>날씨 앱</h2>
+      </Logo>
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
+
+const HeaderWrapper = styled(Container)`
+  font-size: 20px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  @media (max-width: 980px) {
+    flex-direction: column;
+    gap: 20px;
+  }
+`;
+
+const Logo = styled.div`
+  display: flex;
+  flex: 1;
+  justify-content: flex-start;
+  align-items: center;
+  gap: 20px;
+
+  h2 {
+    font-size: 25px;
+    font-weight: 700;
+    line-height: 30px;
+    text-transform: uppercase;
+    color: #4793ff;
+  }
+`;
