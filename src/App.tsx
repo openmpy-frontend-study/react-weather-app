e7e744d570e8890783b0e8a73928c688a7405234
openmpy <suhwan@kakao.com>
2024-09-03 13:44:06 +0900

    feat: Add React Query for data fetching and routing in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+
 function App() {
-  return <div></div>;
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Routes>
+        <Route path="*" element={<Home />} />
+        <Route path="/:id" element={<Home />} />
+      </Routes>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
